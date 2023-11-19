34530e0ef89ba1dc92050222075759307590541f
HWeSek <cichon.adam0205@gmail.com>
2023-11-19 13:37:14 +0100

    Tabela dziala

--- app.js
+++ app.js
@@ -3,13 +3,63 @@ const app = express()
 const PORT = 3000;
 const path = require("path")
 
+let imghelper = function (type) {
+    let src;
+    switch (type) {
+        case "audio/x-flac":
+            src = '774673_format_flac_document_extension_file.png';
+            break;
+        case "image/png":
+            src = '774683_format_file_extension_document_png file firmat.png';
+            break;
+        case "image/jpeg":
+            src = '774688_format_jpg_document_extension_file.png';
+            break;
+        case "application/msword":
+            src = '774674_format_doc_document_extension_file.png';
+            break;
+        case "video/mp4":
+            src = '774675_extension_mp4_format_file_document.png';
+            break;
+        case "text/plain":
+            src = '774680_format_file_extension_document_txt.png';
+            break;
+        case "application/pdf":
+            src = '774684_extension_pdf_document_file_format.png';
+            break;
+        case "audio/mpeg":
+            src = '774685_file_documents_document_mp3_format.png';
+            break;
+        case "application/javascript":
+            src = '774687_file_js file firmat_extension_document_format_file_document_js file firmat_extension_format.png';
+            break;
+        case "text/html":
+            src = '774689_document_extension_format_file_html.png';
+            break;
+        case "image/gif":
+            src = '774690_gif_ducument_format_extension_file_filetype.png';
+            break;
+        default:
+            src = "199231_format_file_extension_blank_icon.png"
+            break;
+    }
+    return `<img src="gfx/icons/${src}"  alt="${type}">`
+}
+
 const hbs = require('express-handlebars');
 app.set('views', path.join(__dirname, 'views'));
 app.engine('hbs', hbs({
     extname: 'hbs',
     defaultLayout: 'main',
-    partialsDir: __dirname + '/views/partials/'
+    partialsDir: __dirname + '/views/partials/',
+    helpers: {
+        idinc: function (id) {
+            return id + 1;
+        },
+        fileImg: imghelper
+    }
 }));
+
 app.set('view engine', 'hbs');
 
 const formidable = require('formidable');
